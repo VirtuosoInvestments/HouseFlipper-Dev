b6b0c4e91356a9740366bd49f3b4faa410b1f7ea
ralph.joachim <ralph.joachim@docusign.com>
2016-08-28 05:11:15 -0700

    Site Registration Sample Binding2

--- Registration/Program.cs
+++ Registration/Program.cs
@@ -13,9 +13,16 @@ namespace HouseFlipper.WebSite.Registration
         {
             string webSiteName = "House Flipper - DEV";
             string physicalPath = @"C:\GitHub\Sites\HouseFlipper\Dev\WebSite";
-            int port = 8080;
-            ServerManager serverManager = new ServerManager();            
-            Site mySite = serverManager.Sites.Add(webSiteName, physicalPath,  port);
+            //int port = 8080;
+            ServerManager serverManager = new ServerManager();
+            //Site mySite = serverManager.Sites.Add(webSiteName, physicalPath,  port);
+
+            //SAMPLE: BINDING
+            string binding1 = "http:192.168.1.100:80:microsoft.com";
+            string protocol = "http";
+            string binding2 = "192.168.1.100:80:microsoft.com";            
+            Site mySite = serverManager.Sites.Add(webSiteName, protocol, binding2, physicalPath);
+
             mySite.ServerAutoStart = true;
             serverManager.CommitChanges();
         }
