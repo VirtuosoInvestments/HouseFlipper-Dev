123a64e82da06c352131c526986dec8f2890096d
ralph.joachim <ralph.joachim@docusign.com>
2016-08-28 05:51:32 -0700

    Site Registration Testing: only got simple binding working

--- Registration/Program.cs
+++ Registration/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,17 +12,45 @@ namespace HouseFlipper.WebSite.Registration
     {
         static void Main(string[] args)
         {
-            string webSiteName = "House Flipper - DEV";
+            string env = "dev";
+            string webSiteName = string.Format("House Flipper - {0}",env.ToUpper());
             string physicalPath = @"C:\GitHub\Sites\HouseFlipper\Dev\WebSite";
             ServerManager serverManager = new ServerManager();
 
-            //BINDING
-            string protocol = "http";
-            string binding = "localhost:80:demo.houseflipper.com";            
-            Site mySite = serverManager.Sites.Add(webSiteName, protocol, binding, physicalPath);
+            //BINDING1 - TRY
+            string protocol = "http";            
+            int port = 8080;
+            //string binding = string.Format("localhost:{0}:{1}.houseflipper.com",port,env);            
+            //Site mySite = serverManager.Sites.Add(webSiteName, protocol, binding, physicalPath);
 
+            //BINDING2 - TRY
+            var ipaddress = "localhost";
+            var hostName = Environment.MachineName;
+            //var binding2 =
+            //string.Format("{0}:{1}:{2}", ipaddress, port, hostName);
+            //Site mySite = serverManager.Sites.Add(webSiteName, protocol, binding2, physicalPath);
+
+            //BINDING3 - TRY
+            var hostName2 = Dns.GetHostName();
+            var ipaddress2 = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0];
+            string binding3 = string.Format(
+                "{0}://{1}:{2}:{3}.houseflipper.com",protocol,ipaddress2,port,env);
+            //Site mySite = serverManager.Sites.Add(webSiteName, binding3, physicalPath, new byte[] { });
+
+            //BINDING4 - TRY
+            string binding4 = string.Format(
+                "{0}://{1}:{2}",protocol,ipaddress,port);
+            //Site mySite = serverManager.Sites.Add(webSiteName, binding4, physicalPath, new byte[] { });
+
+            //BINDING5 - SIMPLE TRY
+            Site mySite = serverManager.Sites.Add(webSiteName, physicalPath,  port);
+
+            //------------------------------------------
             mySite.ServerAutoStart = true;
             serverManager.CommitChanges();
+
+            Console.WriteLine("Program ended. Hit any key to exit!");
+            Console.ReadKey();
         }
     }
 }
