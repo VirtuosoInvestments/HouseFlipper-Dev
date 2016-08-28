d324bccc8b13e9fd8bc73a996f5c7cbebce2b248
ralph.joachim <ralph.joachim@docusign.com>
2016-08-28 04:53:00 -0700

    FEATURE: Site Registration Sample

---
+++ Registration/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Web.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseFlipper.WebSite.Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SAMPLE
+            ServerManager serverManager = new ServerManager();
+            Site mySite = serverManager.Sites.Add("Racing Cars Site", "d:\\inetpub\\wwwroot\racing",  8080);
+            mySite.ServerAutoStart = true;
+            serverManager.CommitChanges();
+        }
+    }
+}
