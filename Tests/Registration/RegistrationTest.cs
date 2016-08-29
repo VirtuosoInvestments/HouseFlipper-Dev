3fbd5f39029770e1337f6e6b7df70a3be7482762
ralph.joachim <ralph.joachim@docusign.com>
2016-08-28 23:19:29 -0700

    REGISTRATION-TEST: Help test - DONE

--- Tests/Registration/RegistrationTest.cs
+++ Tests/Registration/RegistrationTest.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using Test.HouseFlipper.Common;
@@ -55,13 +56,66 @@ namespace Test.HouseFlipper.Registration
             /* Step 2: Read output from file            
              */
             string output = p.StandardOutput.ReadToEnd();
-            Console.WriteLine("Output:\n {0}", output);
+            Console.WriteLine("Standard Output:\n {0}", output);
 
             string error = p.StandardError.ReadToEnd();
-            Console.WriteLine("Error:\n {0}", error);
+            if (!string.IsNullOrWhiteSpace(error))
+            {
+                Console.WriteLine("Standard Error:\n {0}", error);
+            }
 
-            Assert.IsTrue(string.IsNullOrWhiteSpace(error),"Error: Was not expecting error output!");
+            Assert.IsTrue(string.IsNullOrWhiteSpace(error), "Error: Was not expecting error output!");
             Assert.IsTrue(!string.IsNullOrWhiteSpace(output), "Error: Expecting -help would show some output, but appears nothing has been printed to the screen!");
+
+            /* Step 3: Verify output
+             */
+            var expectedOutput = new string[]
+            {                            
+                "HouseFlipper.WebSite.Registration.exe <options>",
+                "OPTIONS:",
+                "-help  Shows help information"
+            };
+
+            List<int> notFound = new List<int>();
+            var actualLines = output.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries );
+            var index = -1;
+            foreach(var eLine in expectedOutput)
+            {
+                ++index;
+                var expected = eLine.ToLower().Trim();
+                var found = false;
+                foreach(var aLine in actualLines)
+                {
+                    if(string.IsNullOrWhiteSpace(aLine))
+                    {
+                        continue;
+                    }
+                    var actual = aLine.Trim().ToLower();
+                    
+                    if(actual.Equals(expected))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found)
+                {
+                    notFound.Add(index);
+                }
+            }
+
+            if(notFound.Count>0)
+            {
+                Console.WriteLine("Error: Following information was not displayed by -help");
+                Console.WriteLine("".PadLeft(50,'-'));
+                foreach(var i in notFound)
+                {
+                    var missing = expectedOutput[i];
+                    Console.WriteLine(missing);
+                }
+                Console.WriteLine();
+                Assert.Fail();
+            }
         }
     }
 }
